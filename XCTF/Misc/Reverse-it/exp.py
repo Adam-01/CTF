535695238a38891bfcb7578428a0790260dd8895
Peiwen Pan <ppw420313439@gmail.com>
2020-09-27 01:31:06 +0800

    add misc & crypto

--- XCTF/Misc/Reverse-it/exp.py
+++
@@ -1,8 +0,0 @@
-with open('Reverse-it', 'rb') as f:
-    with open('Reverse-it.jpg.tmp', 'wb') as fw:
-        fw.write(f.read()[::-1])
-
-with open('Reverse-it.jpg.tmp', 'rb') as f:
-    with open('Reverse-it.jpg', 'wb') as fw:
-        for i in range(100000):
-            fw.write(bytes.fromhex(bytes.hex(f.read(1))[::-1]))
