fdd34ea82f68258958a98bca0a0864f15aad8d9d
Peiwen Pan <ppw420313439@gmail.com>
2020-11-09 04:08:46 +0800

    modify base64stego writeup

--- XCTF/Misc/base64stego/writeup/main.py
+++ XCTF/Misc/base64stego/writeup/main.py
@@ -13,7 +13,7 @@ for line in lines:
 
     index = base64chars.index(lastchar)
     bin_str = bin(index).replace('0b', '').zfill(6)   # 保证二进制串刚好6位
-    flag += bin_str[6 - 2 * num:]     # 取得可用的bits（4个或2个），两个等号可藏4个bit，一个等号可藏2个bit
+    flag += bin_str[-2 * num:]     # 取得可用的bits（4个或2个），两个等号可藏4个bit，一个等号可藏2个bit
 
 # 每8bits凑成一个字节
 i = 0
